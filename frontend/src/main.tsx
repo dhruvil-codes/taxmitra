c633703c6eb9fc8c4df3693f593540bdca2d8e33
Dhruvil Mistry <dhruvilmistry16@gmail.com>
2026-09-04 17:43:26 +0530

    refactor(frontend): entry point cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import "@fontsource/noto-sans-devanagari/400.css";
 import "@fontsource/noto-sans-devanagari/500.css";
 import "@fontsource/noto-sans-devanagari/600.css";
